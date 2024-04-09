76a5ed605c4ade6dc2fbb27f6c4e7a421d9aa037
hitopiyo <nakamurahitoe1129@gmail.com>
2024-04-09 18:08:30 +0900

    first commit

---
+++ site01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MAMPのインストールデモ</title>
+    </head>
+    <body style="background: orange; color: white;">
+        <?php
+            echo 'MAMPのインストールからブラウザでの表示までできたぜ！';
+        ?>
+    </body>
+</html>
