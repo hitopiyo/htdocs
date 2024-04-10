ffec3e1e38afc5cbeaab81f1ea45d0775cc43d08
hitopiyo <nakamurahitoe1129@gmail.com>
2024-04-10 23:59:06 +0900

    PHP3 配列

--- sample.php
+++ sample.php
@@ -7,6 +7,26 @@ echo $a;
 <?php
 $age=20;
 $test="テスト";
-echo $age;
-echo $test;
+echo $age."\n";
+echo $test."\n";
+?>
+
+<?php
+$tan=[1,2,3];
+echo $tan[0]."\n";
+
+$nizi=[
+    ["松下",29,"男性"],
+    ["高田",25,'男性'],
+    ["岡田",25,'女性']
+];
+echo "私の名前は",$nizi[0][0],"です。年齢は",$nizi[0][1],"歳",$nizi[0][2],"です。"."\n";
+
+$ren=
+[
+    'name'=>'田中',
+    'age'=>24,
+    'codelang'=>"HTML"
+];
+echo $ren['name']."\n";
 ?>
