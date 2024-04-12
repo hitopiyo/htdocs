7156a684f391fffa74868401a9e285cf559d7a2e
hitopiyo <nakamurahitoe1129@gmail.com>
2024-04-12 13:57:10 +0900

    PHP 4

--- sample.php
+++ sample.php
@@ -30,3 +30,25 @@ $ren=
 ];
 echo $ren['name']."\n";
 ?>
+
+<?php
+$i=0;
+echo $i."\n";
+echo $i++."\n";
+echo $i."\n";
+echo ++$i."\n";
+?>
+
+<?php
+for($i=1; $i<15 ; $i++){
+    if($i%14===0){
+        echo "for_out"."\n";
+        break;
+    }
+    if($i%2===0){
+        continue;
+    }
+    echo $i."\n";
+    
+}
+?>
